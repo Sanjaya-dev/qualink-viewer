c4e4f2e1c34451c212a4a80bbc4d719faed732f8
Sanjaya <85091736+Sanjaya-dev@users.noreply.github.com>
2025-06-19 22:21:16 +0700

    Update MapView.jsx

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,6 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: '/leaflet/marker-icon-2x.png',
+  iconUrl: '/leaflet/marker-icon.png',
+  shadowUrl: '/leaflet/marker-shadow.png',
+});
+
 export default function MapView({lat,lng}){
     if(!lat || !lng) return <p className="text-red-500">GPS Not Found</p>;
 
@@ -11,7 +19,9 @@ export default function MapView({lat,lng}){
         style={{ height: "200px", width: "100%" }}
         className="rounded overflow-hidden"
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer
+            attribution='&copy; OpenStreetMap contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={[lat, lng]}>
           <Popup>
             location: {lat}, {lng}
